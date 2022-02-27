505cc10e588e3e4002a74677d9f8434161f350b7
ESNEYDER OSPINO <darioospino274539@correo.itm.edu.co>
2022-02-27 12:17:17 -0500

    First step

---
+++ SuperMarket/SuperMarket/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket
+{
+    public class Product
+    {
+        private string _Description;
+        private int _Id;
+        private decimal _Price;
+        private decimal _Tax;
+
+        public Product(string Description,int Id,decimal Price,decimal Tax)
+        {
+            _Description = _Description;
+            _Id = Id;
+            _Price = Price;
+            _Tax = Tax;
+        }
+
+        public override string ToString()
+        {
+            return $"{_Id}\n{_Description}\n{_Price}\n{_Tax}";
+        }
+    }
+}
---
+++ SuperMarket/SuperMarket/Program.cs
@@ -0,0 +1,4 @@
+﻿using SuperMarket;
+
+Product product1 = new Product("Leche",01,2700,5);
+Console.WriteLine(product1);
