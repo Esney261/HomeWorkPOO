f7839c71fb7a35e80817b9217447a163d8608e5b
ESNEYDER OSPINO <darioospino274539@correo.itm.edu.co>
2022-02-27 18:15:34 -0500

    step three

--- SuperMarket/SuperMarket/ComposedProduct.cs
+++ SuperMarket/SuperMarket/ComposedProduct.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,7 @@ namespace SuperMarket
     public class ComposedProduct : Product
     {
         public float Discount { get; set; }
-        public int MyProperty { get; set; }
+        public  ICollection Products { get; set; }
         public override decimal ValueToPay()
         {
             return Price;
